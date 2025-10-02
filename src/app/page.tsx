f9e8dcd1e56056b62d0c23dd4ad3db405e81d815
Bo de Groot <bclm.degroot@gmail.com>
2025-10-02 23:20:51 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import {Grid} from "@mui/system";
 
 export default function Home() {
-  return (
-    <Box></Box>
-  );
-}
+    return (
+        <Box>
+            <Grid container direction="row" spacing={3} sx={{justifyContent: "center", alignItems: "stretch"}}>
+                <Grid size={6}>
+                    <Typography>1</Typography>
+                </Grid>
+                <Grid size={6}>
+                    <Typography>2</Typography>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
